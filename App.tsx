cf67eddc0df7139ace219ff9df9034a9043c3ad0
kelvindominguez1595 <44550467+kelvindominguez1595@users.noreply.github.com>
2022-05-17 15:14:42 -0600

    add files json lottie

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
-import { NavigationContainer } from '@react-navigation/native'
 import React from 'react'
-import { MainStackNavigation } from './src/interfaces/MainStackNavigation'
+import { NavigationContainer } from '@react-navigation/native'
+import { MainStackNavigation } from './src/configs/MainStackNavigation'
 
 export const App = () => {
   return (
---
+++ src/assets/json/index.tsx
@@ -0,0 +1,2 @@
+export const SPLASH = require('./splash.json')
+export const LOADER = require('./loader.json')
